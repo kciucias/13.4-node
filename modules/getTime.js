a2ca11e79372292717704788d80d405d21feafda
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2018-07-30 19:09:15 +0100

    Change time

--- modules/getTime.js
+++ modules/getTime.js
@@ -1,11 +1,22 @@
-
+/*
 function getTime(time) {
-
 	var m = Math.floor(time / 60);
 	var s = time - m * 60;
-
 	return 'Time: ' + m + 'min and ' + s + 's';
 };
+*/
+
+function getTime(time) {
+    var sec_num = parseInt(time, 10); 
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
+}
 
 
 exports.printTime = getTime;
